9ba221e4458eb29fba5259acc0b675740181b9a6
Simon Schmidt <simon.schmidt@mabisile.de>
2020-11-24 11:32:21 +0100

    Initial Commit

---
+++ MikadoMonteCarlo/cmake-build-debug/CMakeFiles/MikadoMonteCarlo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MikadoMonteCarlo.dir/main.cpp.o"
+  "MikadoMonteCarlo"
+  "MikadoMonteCarlo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MikadoMonteCarlo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
