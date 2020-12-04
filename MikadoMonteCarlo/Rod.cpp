4964af4b02316a6fb27af46d06ba403095e01648
nnjjtt <Thorsson@stud.uni-heidelberg.de>
2020-12-04 17:23:14 +0100

    test commit 2

--- MikadoMonteCarlo/Rod.cpp
+++ MikadoMonteCarlo/Rod.cpp
@@ -5,9 +5,7 @@
 #include "Rod.h"
 bool Rod::check_collision (const Rod& other)
 {
-
-	throw "Not implemented yet";
-
+	throw "Not Implemented yet";
 }
 Rod::Rod(double initial_x, double initial_y, double initial_phi, double width, double length)
 {
