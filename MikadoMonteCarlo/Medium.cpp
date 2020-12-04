44a12b1254acfc2acd349bc38e52a22d6cfec88a
Simon Schmidt <simon.schmidt@mabisile.de>
2020-12-04 16:54:38 +0100

    Removed noise

--- MikadoMonteCarlo/Medium.cpp
+++ MikadoMonteCarlo/Medium.cpp
@@ -3,47 +3,9 @@
 //
 
 #include "Medium.h"
-Medium::Medium (const MediumParameters& params):parameters(params)
+Medium::Medium (MediumParameters parameters)
+	: parameters(parameters)
+{
 
-{
-	throw "Not Implemented yet";
-}
-void Medium::initialize_rods()
-{
-	throw "Not Implemented yet";
-}
-double Medium::calculate_energy() const
-{
-	throw "Not Implemented yet";
-}
-TrialMedium Medium::get_trial_medium()
-{
-	return TrialMedium(parameters, rods);
-}
-void Medium::update(TrialMedium trial_medium)
-{
-	rods[trial_medium.get_changed_rod_index()] = trial_medium.get_changed_rod();
-}
-TrialMedium::TrialMedium(const MediumParameters &params, const std::vector<Rod> &previous_rods) : parameters(&params)
-{
-	rods = std::vector<std::reference_wrapper<Rod>>(previous_rods.begin(),previous_rods.end());
-}
-void TrialMedium::random_movement(const double &time_step, std::mt19937 rng)
-{
-	std::uniform_int_distribution<> distrib(0, rods.size()-1);
-	changed_rod_index = distrib(rng);
-	Rod new_rod = move_random(rods[changed_rod_index],time_step,rng);
-	rods[changed_rod_index] = std::ref(new_rod);
-}
-double TrialMedium::calculate_energy() const
-{
-	throw "Not Implemented yet";
-}
-Rod TrialMedium::move_random(Rod r, const double &time_step, std::mt19937 rng)
-{
-	throw "Not Implemented yet";
-}
-//This function generates the output which is then written to a file
-std::ostream &operator<<(std::ostream &os, Medium const &m) {
 	throw "Not Implemented yet";
 }
