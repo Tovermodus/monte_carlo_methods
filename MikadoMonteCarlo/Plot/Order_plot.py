522a117cc1d09ea700f5a31b7c8d3b33686b9548
nnjjtt <74024011+nnjjtt@users.noreply.github.com>
2021-01-21 15:35:25 +0100

    Added rejection estimation plot

--- MikadoMonteCarlo/Plot/Order_plot.py
+++ MikadoMonteCarlo/Plot/Order_plot.py
@@ -7,6 +7,19 @@ import matplotlib.pyplot as plt
 
 
 
+class Rod:
+    def __init__(self, x, y, phi, length, width, domain_w, domain_h):
+        self.x = x
+        self.y = y
+        self.phi = phi
+        self.length = length
+        self.width = width
+
+
+
+old_rods = [Rod(0, 0, 0, 0, 0, 0, 0 )]
+
+
 
 def order_parameter(rods):
     angles=np.array([r.phi for r in rods])
@@ -23,18 +36,37 @@ else:
 fileno=re.search(r'(?<=iterations:)\S*?(?=-)',filename).group(0)
 open(filename+"/order_parameter.txt", 'w').close()
 
+
 def plot_order():
     with open(filename+"/order_parameter.txt", 'r') as f:
-        order_p=np.array(f.readlines(), dtype=float)
+        #print(f.readlines())
+        tmp=np.array([el.split(';') for el in f.readlines()])
+        #print(tmp)
+        order_p=np.array(tmp[:,0], dtype=float)
+        rej_p=np.array(tmp[:,1], dtype=float)
         plt.plot(order_p)
         plt.title(r'Order parameter $S_f= \frac{1}{N} \sum_{i=0}^N 2*cos^2(\phi_i)-1$')
         plt.xlabel('timesteps')
         plt.ylabel('order parameter')
         plt.savefig(filename+"/order_parameter.png")
         plt.show()
+        fig, ax = plt.subplots(2, sharex='col')
+        ax[0].plot(order_p[1:])
+        ax[0].set_title(r'Order parameter $S_f= \frac{1}{N} \sum_{i=0}^N 2*cos^2(\phi_i)-1$', y=1.05)
+        ax[0].set_xlabel('timesteps')
+        ax[0].set_ylabel('order parameter')
+        ax[1].plot(rej_p[1:])
+        ax[1].set_title(r'rejection rate estimation')
+        ax[1].set_xlabel('timesteps')
+        ax[1].set_ylabel('unchanged rods over plotinterval')
+        fig.set_size_inches(10,10)
+        plt.savefig(filename+"/rejection_parameter.png")
+        plt.show()
 
 def plot_file(name):
     rods = []
+
+    global old_rods
     f = open(name)
     lines = f.readlines()
     whline = lines[0][:-1]
@@ -45,24 +77,26 @@ def plot_file(name):
     ws,ls =lwline.split(' ')
     l = float(ls)
     w = float(ws)
-    for i in range(2,len(lines)):
-        x,y,phi = lines[i].split(' ')
-        rods.append(Rod(float(x),float(y),float(phi),l,w,dw,dh))
+    same_count=0
+    for i in range(2, len(lines)):
+        x, y, phi = lines[i].split(' ')
+        x, y, phi =float(x), float(y), float(phi)
+        if len(old_rods) > 1:
+            if (x == old_rods[i-2].x) and (y == old_rods[i-2].y) and (phi == old_rods[i-2].phi):
+                same_count += 1
+        rods.append(Rod(x,y,phi,l,w,dw,dh))
+
 
     with open(filename+"/order_parameter.txt", 'a') as f:
-        f.write(str(order_parameter(rods))+"\n")
+        f.write(str(order_parameter(rods))+';'+str(same_count)+"\n")
     f.close()
+    #print(len(old_rods),len(rods))
+    old_rods = rods
 
 
 
 
-class Rod:
-    def __init__(self, x, y, phi, length, width, domain_w, domain_h):
-        self.x = x
-        self.y = y
-        self.phi = phi
-        self.length = length
-        self.width = width
+
 
 for i in range(int(fileno)):
     plot_file(filename+"/"+str(i)+".txt")
